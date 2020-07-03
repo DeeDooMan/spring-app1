eec1463306cf35df7383bbe097f4243d10dbf34d
Atai Isaev <atai.b.isaev@gmail.com>
2020-07-03 23:59:40 +0600

    Dependency Injection #HomeWork

--- src/main/java/kg/isaev/springcources/MusicPlayer.java
+++ src/main/java/kg/isaev/springcources/MusicPlayer.java
@@ -1,25 +1,36 @@
 package kg.isaev.springcources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {
     }
 
     public void playMusic(){
-        System.out.println("Playing:  "+music.getSong());
+        if(musicList.isEmpty()){
+            System.out.println("MusicPlayer has not any music!");
+        }
+        else{
+            for (Music music: musicList) {
+                System.out.println("Playing:  "+music.getSong());
+            }
+        }
+
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public String getName() {
